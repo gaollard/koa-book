59b2701ce1238748e981aeb19915a282f9857256
trustprocess <trustprocess@foxmail.com>
2018-06-05 19:49:15 +0800

    use browser history mode

---
+++ src/public/static/js/1.aed6d2822ab32e12c68e.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{"9VZ2":function(e,t){},B64m:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var a={render:function(){var e=this.$createElement;return(this._self._c||e)("div",{staticClass:"wrapper"},[this._v("\n  众人忠哥\n")])},staticRenderFns:[]};var r=n("VU/8")({name:"Account"},a,!1,function(e){n("9VZ2")},"data-v-65ef95a1",null);t.default=r.exports}});
+//# sourceMappingURL=1.aed6d2822ab32e12c68e.js.map
